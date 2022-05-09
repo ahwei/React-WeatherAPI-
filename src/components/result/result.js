87c46161d455a97b6e17205164080b663b95d6e0
AhWeiHENGuEi <aa9440307@gmail.com>
2022-05-09 23:17:45 +0800

    add weather card

--- src/components/result/result.js
+++ src/components/result/result.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import { Grid, Typography, Avatar } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import { useSelector, useDispatch } from "react-redux";
-import { classes } from "istanbul-lib-coverage";
+import WeatherCard from "./weatherCard";
 
 const Result = (props) => {
   const classes = useStyles();
@@ -22,32 +22,7 @@ const Result = (props) => {
         </Grid>
       )}
 
-      {today && today?.status === 200 && (
-        <Grid sx={{ paddingLeft: 3 }}>
-          <Typography variant="body1">
-            {today?.location?.name},{today?.location?.country}
-          </Typography>
-          <Grid container>
-            <Avatar
-              src={`http://openweathermap.org/img/wn/${today?.weather[0].icon}@2x.png`}
-              sx={{ width: 56, height: 56 }}
-            />
-            <Typography variant="h3">{today?.weather[0].main}</Typography>
-          </Grid>
-          <Typography variant="body2">
-            Description:{today?.weather[0].description}
-          </Typography>
-          <Typography variant="body2">
-            Temperature:{today?.main?.temp_min}°C~{today?.main?.temp_max}°C
-          </Typography>
-          <Typography variant="body2">
-            Humidity:{today?.main?.humidity}%
-          </Typography>
-          <Typography variant="body2">
-            {/* Time:{today?.weather[0].description} */}
-          </Typography>
-        </Grid>
-      )}
+      {today && today?.status === 200 && <WeatherCard today={today} />}
     </Grid>
   );
 };
--- src/components/result/weatherCard.js
+++ src/components/result/weatherCard.js
@@ -1,9 +1,41 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Grid, Typography, Avatar } from "@mui/material";
+import { makeStyles } from "@mui/styles";
 
-const weatherCard = (props) => {
-  return <div>weatherCard</div>;
+const weatherCard = ({ today }) => {
+  return (
+    <Grid sx={{ paddingLeft: 3 }}>
+      <Typography variant="body1">
+        {today?.location?.name},{today?.location?.country}
+      </Typography>
+      <Grid container>
+        <Avatar
+          src={`http://openweathermap.org/img/wn/${today?.weather[0].icon}@2x.png`}
+          sx={{ width: 56, height: 56 }}
+        />
+        <Typography variant="h3">{today?.weather[0].main}</Typography>
+      </Grid>
+      <Typography variant="body2">
+        Description:{today?.weather[0].description}
+      </Typography>
+      <Typography variant="body2">
+        Temperature:{today?.main?.temp_min}°C~{today?.main?.temp_max}°C
+      </Typography>
+      <Typography variant="body2">Humidity:{today?.main?.humidity}%</Typography>
+      <Typography variant="body2">
+        {/* Time:{today?.weather[0].description} */}
+      </Typography>
+    </Grid>
+  );
 };
+
+const useStyles = makeStyles((theme) => ({
+  notfound: {
+    background: "#ff00006e",
+    border: "1px solid red",
+  },
+}));
 
 weatherCard.propTypes = {};
 
